7c02a38b1a5dafe9fff407de275c1bcb3a40b7ca
John Tillman <johntillman@Johns-MacBook-Pro.local>
2018-09-20 19:56:53 -0400

    test

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ var db = require("./models");
 
 var app = express();
 var PORT = process.env.PORT || 3000;
+console.log('hello')
 
 // Middleware
 app.use(bodyParser.urlencoded({ extended: false }));
