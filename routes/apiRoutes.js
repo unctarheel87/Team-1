4aee560e83b1c2c5c8e283e35baa6fe3bd399a91
Jennifer Fahey <jenguin777@gmail.com>
2018-09-22 16:36:13 -0400

    added sequelize to apiRoutes.js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,15 +1,73 @@
-var db = require("../models");
+// *********************************************************************************
+// api-routes.js - this file offers a set of routes for displaying and saving data to the db
+// *********************************************************************************
 
+// Dependencies
+// =============================================================
+
+// go use index.js to fetch all models (all .js files) in the models folder and put them in the db.
+var db = require("../models");
+var express = require('express');
+
+// Routes
+// =============================================================
 module.exports = function(app) {
+
+// Create all our routes and set up logic within those routes where required.
+  // Finding all Burgers, and then returning them to the user as JSON.
+  // Sequelize queries are asynchronous, which helps with perceived speed.
+  // If we want something to be guaranteed to happen after the query, we'll use
+  // the .then function
   app.get("/", function(req, res) {
-    db.User.findAll({}).then(function(response) {
-      res.json(response);
-    });
-    // db.Interest.findAll({}).then(function(response) {
-    //   res.json(response);
-    // });
-    // db.Message.findAll({}).then(function(response) {
-    //   res.json(response);
-    // });
+        db.User.findAll({}).then(function(results){
+            // results are available to us inside the .then
+            
+            res.render("index", { users: results });
+            
+        }).catch(function(error) {
+            throw error;
+        }); 
   });
+
+  // add a burger
+  app.post("/api/users", function(req, res) {
+
+        // adding server-side validation for burger_name not null and burger_name < 36 chars
+        if(req.body.name === '' || req.body.name.length > 35) {
+        
+        return res.status(404).json('name validation failed').end();
+        } else { // if it passes validation, insert the burger
+
+            db.User.create({
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            userName: req.body.userName,
+            password: req.body.password,
+            image: req.body.image
+            created_at: req.body.created_at
+            }).then(function(results) {
+            // Send back the ID of the new burger
+           
+            res.json({ id: results.insertId });
+            });
+        }
+  });
+  
+  app.put("/api/users/:id", function(req, res) {
+
+    console.log("req.params.id: " + req.params.id);
+
+        db.User.update({
+            firstName: req.body.firstName  
+        }, {where: {id: req.body.id }
+        })
+        .then(function(results){
+                if (results.changedRows == 0) {
+                    // If no rows were changed, then the ID must not exist, so 404
+                    return res.status(404).end();
+                } else {
+                    res.status(200).end();
+                }
+        });
+    });
 };
