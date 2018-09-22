f12c0a1e140149acc375235856c227425b7924bd
John Tillman <john.w.tillman@gmail.com>
2018-09-22 14:02:49 -0400

    changes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,6 +3,9 @@ var path = require('path');
 
 module.exports = function(app) {
   app.get("/", function(req, res) {
+    db.User.find({}).then(response => {
+      console.log(response);
+    })
     res.send('index.html');
   });
 };
