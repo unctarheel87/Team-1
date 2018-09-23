28c68cc9b71af2d0c5fd25c8e2d8177cbb24f81e
John Tillman <john.w.tillman@gmail.com>
2018-09-23 14:48:29 -0400

    api routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,11 +1,79 @@
+var router = require("express").Router();
 var db = require("../models");
-var path = require('path');
 
-module.exports = function(app) {
-  app.get("/", function(req, res) {
-    db.User.find({}).then(response => {
-      console.log(response);
+router.get("/", (req, res) => {
+  res.sendFile("index.html");
+});
+
+router.get("/api/users", (req, res) => {
+  db.User.findAll({})
+    .then(response => {
+      res.json(response);
     })
-    res.send('index.html');
-  });
-};
+    .catch(err => {
+      console.log(err);
+      res.status(500).end();
+    });
+});
+
+//create user
+router.post("/api/users", (req, res) => {
+  const newUser = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    userName: req.body.userName,
+    password: req.body.password,
+    img: req.body.img
+  };
+  db.User.create(newUser)
+    .then(response => {
+      console.log(response);
+      res.status(200).end();
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).end();
+    });
+});
+
+router.put("/api/users/:id", (req, res) => {
+  const newUser = {
+    username: req.body.username,
+    password: req.body.password
+  };
+  db.User.update(newUser, { 
+    where: { id: req.params.id }
+  })
+    .then(response => {
+      if (response.changedRows === 0) {
+        return res.status(404).end();
+      } else {
+        res.status(200).end();
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).end();
+    });
+});
+
+router.delete("/api/users/:id", (req, res) => {
+  db.User.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(response => {
+      if (response.changedRows === 0) {
+        return res.status(404).end();
+      } else {
+        res.status(200).end();
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).end();
+    });
+});
+
+module.exports = router;
