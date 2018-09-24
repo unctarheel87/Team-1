b29a3c79f9143a96c551c00cba320f068ebbd33b
John Tillman <john.w.tillman@gmail.com>
2018-09-24 10:59:46 -0400

    passport routes

--- public/js/index.js
+++ public/js/index.js
@@ -25,6 +25,18 @@ const app = new Vue({
         .catch(function(error) {
           console.log(error);
         });
+    },
+    handleSubmit(event) {
+      event.preventDefault();
+      let formData = {
+        username: event.target.username.value,
+        password: event.target.password.value
+      };
+      console.log(formData);
+      axios
+        .post("/api/login", formData)
+        .then(response => console.log(response))
+        .catch(err => console.log(err));
     }
   }
 });
