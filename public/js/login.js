1058306745d8a13f0c7dfb57469005597f35646b
John Tillman <john.w.tillman@gmail.com>
2018-09-27 19:50:36 -0400

    test

--- public/js/login.js
+++ public/js/login.js
@@ -21,7 +21,10 @@ new Vue({
       console.log(formData);
       axios
         .post("/api/users", formData)
-        .then(response => console.log(response))
+        .then(response => {
+          console.log(response);
+          $("#modal2").modal("close");
+        })
         .catch(err => console.log(err));
     },
     handleLogin(event) {
@@ -34,6 +37,7 @@ new Vue({
       axios
         .post("/api/login", formData)
         .then(response => {
+          $("#modal1").modal("close");
           window.location.replace(response.data);
         })
         .catch(err => console.log(err));
