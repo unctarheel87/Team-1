3228c299b5527c200e5454f3a897d2f7b391c6ff
John Tillman <john.w.tillman@gmail.com>
2018-09-26 22:27:51 -0400

    login

--- public/js/index.js
+++ public/js/index.js
@@ -15,17 +15,6 @@ const app = new Vue({
           console.log(error);
         });
     },
-    getData2: function() {
-      axios
-        .get("/api/users")
-        .then(response => {
-          console.log(response);
-          this.message = JSON.stringify(response.data[0].firstName, null, 2);
-        })
-        .catch(function(error) {
-          console.log(error);
-        });
-    },
     handleSubmit(event) {
       event.preventDefault();
       let formData = {
@@ -38,6 +27,18 @@ const app = new Vue({
       console.log(formData);
       axios
         .post("/api/users", formData)
+        .then(response => console.log(response))
+        .catch(err => console.log(err));
+    },
+    handleLogin(event) {
+      event.preventDefault();
+      let formData = {
+        username: event.target.username.value,
+        password: event.target.password.value
+      };
+      console.log(formData);
+      axios
+        .post("/api/login", formData)
         .then(response => console.log(response))
         .catch(err => console.log(err));
     }
